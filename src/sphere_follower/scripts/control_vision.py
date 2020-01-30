900e4bc5157eaad02b5bf7183c6c772851583bc6
diogo <diogomartins.ac@gmail.com>
2020-01-29 21:49:52 -0300

    control_vision

--- src/sphere_follower/scripts/control_vision.py
+++ src/sphere_follower/scripts/control_vision.py
@@ -2,6 +2,11 @@
 import rospy
 from geometry_msgs.msg import Vector3
 from geometry_msgs.msg import Twist
+from geometry_msgs.msg import Quaternion
+from nav_msgs.msg import Odometry
+
+from sensor_msgs.msg import CameraInfo
+
 import time
 
 from control_pid import ControlPid
@@ -11,31 +16,65 @@ class ControlVision:
   control_pid_yaw = None
   pub_cmd_vel = None
   msg_twist = None
+  camera_info = None
+  pub_quaternion = None
+  odometry_data = None
+  rpy_angles_data = None
 
   def __init__ (self):
+    rospy.loginfo("INIT CONTROL VISION")
     rospy.init_node("robot_vision", anonymous=True)
     self.pub_cmd_vel = rospy.Publisher("/cmd_vel", Twist, queue_size=1)
+    
+    self.pub_quaternion = rospy.Publisher("/rotation_quaternion", Quaternion, queue_size=1)
+    rospy.Subscriber("/odometry/filtered", Odometry, self.callback_odometry)
+    
+    rospy.Subscriber("/rpy_angles", Vector3, self.callback_rpy_angles)
+    
     self.control_pid_x = ControlPid(5, -5, 0.01, 0, 0)
     self.control_pid_yaw = ControlPid(3, -3, 0.001, 0, 0)
     self.msg_twist = Twist()
+    rospy.Subscriber("/diff/camera_top/camera_info", CameraInfo, self.callback_camera_info)
        
   def callback(self, data):
     if data.x != -1:
-      self.msg_twist.angular.z = self.control_pid_yaw.pid_calculate(0.5, 600, int(data.x))
+      self.msg_twist.angular.z = self.control_pid_yaw.pid_calculate(0.5, self.camera_info.width/2, int(data.x))
       self.msg_twist.linear.x = self.control_pid_x.pid_calculate(0.5, 180, int(data.z))
       self.pub_cmd_vel.publish(self.msg_twist)
 
+  def callback_camera_info(self, data):
+    self.camera_info = data
+  
+  def callback_odometry(self, data):
+    self.odometry_data = data
+    quaternion = Quaternion()
+    quaternion.x = data.pose.pose.orientation.x
+    quaternion.y = data.pose.pose.orientation.y
+    quaternion.z = data.pose.pose.orientation.z
+    quaternion.w = data.pose.pose.orientation.w
+    position = Vector3()
+    position.x = data.pose.pose.position.x
+    position.y = data.pose.pose.position.y
+    position.z = data.pose.pose.position.z
+    self.pub_quaternion.publish(quaternion)
+  
+  def callback_rpy_angles(self, data):
+    self.rpy_angles_data = data
+    rpy = Vector3()
+    rpy.x = data.x
+    rpy.y = data.y
+    rpy.z = data.z
+    rospy.loginfo(rpy)
+
+  def sphere_coordinate(self, position, rpy)
+    
 
   def run(self):
     self.msg = rospy.Subscriber("/camera/obj/coordinates", Vector3, self.callback)
 
 if __name__ == "__main__":
-  rospy.loginfo("Init Vision")
+  rospy.loginfo("Init Control")
   ctrl_vision = ControlVision()
   ctrl_vision.run()
   while not rospy.is_shutdown():
-    rospy.spin()
-    
-
-
-    
+    rospy.spin()    
