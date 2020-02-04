3348eb8cee38c7dc41121f13b33cf330d6324af3
diogo <diogomartins.ac@gmail.com>
2020-02-03 21:38:19 -0300

    home ajustments

--- src/challenge_husky/scripts/mission_node.py
+++ src/challenge_husky/scripts/mission_node.py
@@ -152,14 +152,14 @@ class Robot:
     distance = (1 * self.focalLength) / (radius * 2)
     y_move_base = -(center_ball - self.camera_info.width/2) / (radius*2) 
     x_move_base = math.sqrt(distance**2 - y_move_base**2)
-    self.distance_filtered = 0.7*self.distance_filtered + 0.3*distance
-    self.x_move_base_filtered = 0.7*self.x_move_base_filtered + 0.3*x_move_base
-    self.y_move_base_filtered = 0.7*self.y_move_base_filtered + 0.3*y_move_base
-    self.msg_move_to_goal.pose.position.x = self.x_move_base_filtered
-    self.msg_move_to_goal.pose.position.y = self.y_move_base_filtered
+    self.distance_filtered = 0.6*self.distance_filtered + 0.4*distance
+    self.x_move_base_filtered = 0.6*self.x_move_base_filtered + 0.4*x_move_base
+    self.y_move_base_filtered = 0.6*self.y_move_base_filtered + 0.4*y_move_base
+    self.msg_move_to_goal.pose.position.x = x_move_base
+    self.msg_move_to_goal.pose.position.y = y_move_base
     self.msg_move_to_goal.pose.orientation.w = 1
     self.msg_move_to_goal.header.frame_id = self.camera_info.header.frame_id
-    if self.flag and abs(distance- self.distance_filtered) < 5 and self.distance_filtered > 4:
+    if self.flag and abs(distance- self.distance_filtered) < 7 and self.distance_filtered > 4:
       self.pub_move_to_goal.publish(self.msg_move_to_goal)
       self.flag = False
       self.timer_flag = time.time()
